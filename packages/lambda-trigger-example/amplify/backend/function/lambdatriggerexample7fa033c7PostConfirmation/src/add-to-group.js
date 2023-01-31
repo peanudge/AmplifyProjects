89d6d2bc110a791d5d342cc717bde04505f1f3fe
peanut-lover <jihoson94@gmail.com>
2023-01-31 14:03:41 +0900

    Add Auth Service with adding user to group(admin)

---
+++ packages/lambda-trigger-example/amplify/backend/function/lambdatriggerexample7fa033c7PostConfirmation/src/add-to-group.js
@@ -0,0 +1,53 @@
+const aws = require("aws-sdk");
+
+const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({
+  apiVersion: "2016-04-18",
+});
+
+/**
+ * @type {import('@types/aws-lambda').PostConfirmationTriggerHandler}
+ */
+exports.handler = async (event) => {
+  const cognitoProvider = new aws.CognitoIdentityServiceProvider({
+    apiVersion: "2016-04-18",
+  });
+
+  let isAdmin = false;
+  const adminEmails = ["jihoson94@gmail.com"];
+
+  if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
+    isAdmin = true;
+  }
+
+  const groupParams = {
+    UserPoolId: event.userPoolId,
+  };
+
+  const userParams = {
+    UserPoolId: event.userPoolId,
+    Username: event.userName,
+  };
+
+  if (!isAdmin) {
+    return event;
+  }
+
+  groupParams.GroupName = "Admin";
+  userParams.GroupName = "Admin";
+  /**
+   * Check if the group exists; if it doesn't, create it.
+   */
+  try {
+    await cognitoidentityserviceprovider.getGroup(groupParams).promise();
+  } catch (e) {
+    await cognitoidentityserviceprovider.createGroup(groupParams).promise();
+  }
+  /**
+   * Then, add the user to the group.
+   */
+  await cognitoidentityserviceprovider
+    .adminAddUserToGroup(addUserParams)
+    .promise();
+
+  return event;
+};
---
+++ packages/lambda-trigger-example/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "lambdatriggerexample7fa033c7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "lambdatriggerexample7fa033c7PostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
