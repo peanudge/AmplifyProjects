1204357814696f6806302b9f4552ab160b2d8001
peanut-lover <jihoson94@gmail.com>
2023-01-31 15:19:54 +0900

    Sample UI

--- packages/lambda-trigger-example/src/App.tsx
+++ packages/lambda-trigger-example/src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CognitoUser } from "@aws-amplify/auth";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Auth } from "aws-amplify";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, updateUser] = useState<CognitoUser | null>(null);
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((user) => updateUser(user))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  let isAdmin = false;
+  if (user) {
+    const session = user.getSignInUserSession();
+    const idToken = session?.getIdToken();
+    const payload = idToken?.payload!;
+    console.log("payload: ", payload);
+    if (
+      payload["cognito:groups"] &&
+      (payload["cognito:groups"] as string[]).includes("Admin")
+    ) {
+      isAdmin = true;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Hello world</h1>
+        {isAdmin && <p>Welcome, Admin</p>}
       </header>
+      <button onClick={() => Auth.signOut()}>Sign Out</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
